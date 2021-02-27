cf240c6e0b3536ccf024e8b811c61dfd3f302c10
Kaique Dirceu <dirceukaique@gmail.com>
2021-02-27 12:08:00 -0300

    add notification

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,6 +1,10 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
 import styles from '../styles/components/Profile.module.css';
 
 export function Profile() {
+  const { level } = useContext(ChallengesContext);
+
   return (
     <div className={styles.profileContainer}>
       <img src="https://github.com/kaique-dirceu.png" alt="Kaique"/>
@@ -10,7 +14,7 @@ export function Profile() {
         </strong>
         <p>
           <img src="icons/level.svg" alt="Level"/>
-          Level 1
+          Level {level}
         </p>
       </div>
     </div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,6 +7,7 @@ import Head from 'next/head';
 
 import styles from '../styles/pages/Home.module.css';
 import { ChallengeBox } from "../components/ChallengeBox";
+import { CountdownProvider } from "../contexts/CountdownContext";
 
 export default function Home() {
   return (
@@ -16,6 +17,7 @@ export default function Home() {
       </Head>
       <ExperienceBar/>
     
+    <CountdownProvider>
       <section>
         <div>
           <Profile />
@@ -26,6 +28,7 @@ export default function Home() {
           <ChallengeBox/>
         </div>
       </section>
+    </CountdownProvider>
     </div>
   )
 }
